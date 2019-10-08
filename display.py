188904f81dfb7ee91ad15edf1795da88848502e8
Veerain Grover <vgveerain@gmail.com>
2019-10-08 13:47:29 +0530

    Display Class created; The video is RGB Finally; TimeStamp - 45:26

--- display.py
+++ display.py
@@ -15,6 +15,6 @@ class Display(object):
         for event in events:
             if event.type == sdl2.SDL_QUIT:
                 exit(0)
-        surf = sdl2.ext.pixels2d(self.window.get_surface())
-        surf[:] = img.swapaxes(0,1)[:, :, 0]
+        surf = sdl2.ext.pixels3d(self.window.get_surface())
+        surf[:, :, 0:3] = img.swapaxes(0,1)
         self.window.refresh()
